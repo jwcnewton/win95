187b9febe36d8bcd3295f4cb7893f75ce6eff1ee
Jonathan Newton <Jonathan@jonathans-mbp.home>
2017-05-09 23:24:36 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,13 @@
+var fs = require("fs");
+var host = "127.0.0.1";
+var port = 4200;
+var express = require("express");
+
+var app = express();
+app.use(express.static(__dirname)); //use static files in ROOT/public folder
+
+app.get("/", function(request, response){ //root dir
+    response.send("Hello!!");
+});
+
+app.listen(port, host);
---
+++ scripts/desktop.js
@@ -0,0 +1,7 @@
+$(document).on("ready", function() {
+
+    $("#recycleIcon").draggable({ grid: [80, 80] });
+    $("#networkIcon").draggable({ grid: [80, 80] });
+    $("#windowsIcon").draggable({ grid: [80, 80] });
+    $("#computIcon").draggable({ grid: [80, 80] });
+});
