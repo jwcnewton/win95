5ead1a3c9ec59622ba9abade13e8f1d18616dcde
jwcnewton <jwcnewton@gmail.com>
2017-05-10 16:28:01 +0100

    Updated CSS via upload

--- scripts/desktop.js
+++ scripts/desktop.js
@@ -4,4 +4,18 @@ $(document).on("ready", function() {
     $("#networkIcon").draggable({ grid: [80, 80] });
     $("#windowsIcon").draggable({ grid: [80, 80] });
     $("#computIcon").draggable({ grid: [80, 80] });
+
+   document.getElementById("desktop-clock").innerText = getTime();
 });
+
+setInterval(function(){
+
+   document.getElementById("desktop-clock").innerText = getTime();
+}, 10000);
+
+function getTime(){
+
+    var localTime = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+
+    return localTime;
+}
